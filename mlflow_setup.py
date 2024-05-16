3a70858b287a7dc47fd3f8c52038f5f2ca968cad
K14aNB <kirnb@protonmail.com>
2024-05-16 16:53:54 +0530

    modified path to env variables file into a standard path

--- mlflow_setup.py
+++ mlflow_setup.py
@@ -4,7 +4,7 @@ import os
 from dotenv import load_dotenv
 import errno
 
-def setup_with_dagshub(repo_owner:str,repo_name:str,experiment_name:str,runtime:str,dotenv_path=None):
+def setup_with_dagshub(repo_owner:str,repo_name:str,experiment_name:str,runtime:str):
     '''
     Performs MLFlow setup with Dagshub as tracking server
 
@@ -13,8 +13,6 @@ def setup_with_dagshub(repo_owner:str,repo_name:str,experiment_name:str,runtime:
     repo_name:str: Dagshub repo name
     experiment_name:str: Experiment name in Dagshub tracking server
     runtime:str: Indicates currently active runtime type. Can be colab, jupyter or python-script
-    dotenv_path:str: Optional argument which is required if using local runtime (jupyter or python-script)
-                     default is None
     '''
     
     if mlflow.is_tracking_uri_set() is True:
@@ -25,11 +23,8 @@ def setup_with_dagshub(repo_owner:str,repo_name:str,experiment_name:str,runtime:
             os.environ['MLFLOW_TRACKING_USERNAME']=userdata.get('MLFLOW_TRACKING_USERNAME')
             os.environ['MLFLOW_TRACKING_PASSWORD']=userdata.get('MLFLOW_TRACKING_PASSWORD')
         elif runtime in ['jupyter','python-script']:
-            if dotenv_path is not None :
-                if '~' in dotenv_path:
-                    dotenv_path=dotenv_path.split('~/')[1]
-                    dotenv_path=os.path.join(os.path.expanduser('~'),dotenv_path)
-                load_dotenv(dotenv_path=dotenv_path)
+            if os.path.isfile(os.path.join(os.path.expanduser('~'),'.env',repo_name,'environment_variables.env')):
+                load_dotenv(dotenv_path=os.path.join(os.path.expanduser('~'),'.env',repo_name,'environment_variables.env'))
             else:
                 raise FileNotFoundError(errno.ENOENT,os.strerror(errno.ENOENT),'.env')
         
